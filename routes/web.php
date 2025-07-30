8b039e202cc6122a79ae3513e5068087775b91bb
inving9378 <82240997+inving9378@users.noreply.github.com>
2025-07-30 17:56:29 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,15 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/fallas', [FallaAdminController::class, 'index'])->name('admin.fallas.index');
     Route::post('/fallas/{id}/responder', [FallaAdminController::class, 'responder'])->name('admin.fallas.responder');
 });
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Si tienes autenticación Laravel UI, Breeze o Jetstream, suele incluir:
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
